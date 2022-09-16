b72aa16ec3ada1d9376c60e738cab91edfefd840
Tyler <tylermwight@gmail.com>
2022-09-16 04:45:25 +0000

    add, del, and check for duplicates

--- discord_mysql.py
+++ discord_mysql.py
@@ -17,71 +17,77 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
-tables = []
+#tables = []
 
-cursor.execute("SELECT DATABASE()")
-for x in cursor:
-    db = x
-cursor.execute("SHOW TABLES")
-for x in cursor:
-    tables.append(x)
+#cursor.execute("SELECT DATABASE()")
+#for x in cursor:
+#    db = x
+#cursor.execute("SHOW TABLES")
+#for x in cursor:
+#    tables.append(x)
 #strdb = ''
 #strdb = db[0]
 #strtables = ''
 #strtables = tables[0]
-print(type(db))
-db = ''.join(db)
-tables = ''.join(''.join(tup) for tup in tables)
-print(type(db))
-print(type(tables))
-print(db)
-print(tables)
-#print(tables)
+#db = ''.join(db)
+#tables = ''.join(''.join(tup) for tup in tables)
 
-
+#====================
+#discord auth
+#===================
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 #intents = discord.Intents(messages=True, guilds=True)
 intents = discord.Intents.all()
-
-
-client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="!",intents=intents)
 
 
 @bot.command(brief="add my account to the list")
 async def add_me(ctx):
     await ctx.send(f'I am trying to add {ctx.author.name} to the DB')
-    sql = "INSERT INTO accounts (account_id,name) VALUES (%s, %s)"
-    print(ctx.author.id)
-    print(ctx.author.name)
-    print(type(ctx.author.id))
-    print(type(ctx.author.name))
-    val = (ctx.author.id, ctx.author.name)
-    cursor.execute(sql, val)
+    duplicate = 0
+    cursor.execute("SELECT * FROM accounts")
+    sql_out = []
+    for x in cursor:
+        for y in x:
+            if str(ctx.author.id) in y:
+                print("duplicate user found!")
+                duplicate = duplicate + 1
+    
+
+    if (duplicate == 0):
+        sql = "INSERT INTO accounts (account_id,name) VALUES (%s, %s)"
+        val = (ctx.author.id, ctx.author.name)
+        cursor.execute(sql, val)
+        mydb.commit()
+        print(cursor.rowcount, "was inserted")
+        await ctx.send(f'I inserted {cursor.rowcount} rows using {ctx.author.id} and {ctx.author.name}')
+    elif (duplicate == 1):
+        await ctx.send(f'I found a duplicate so I did not add user to the DB')
+    else:
+        await ctx.send(f'I found more than one duplicate of the same user, trying to delete (not yet gotat code that part)')
+        sql = "DELETE FROM accounts WHERE account_id = {} limit 1".format(ctx.author.id)
+        cursor.execute(sql)
+        mydb.commit()
+        print(cursor.rowcount, "records deleted")
+        await ctx.send(f'I deleted {cursor.rowcount} records with the id {ctx.author.id}')
+
+
+@bot.command(brief="remove my account from the list")
+async def del_me(ctx):
+    await ctx.send(f'I am trying to delete {ctx.author.name} from the DB')
+    cursor.execute("SELECT * FROM accounts")
+    for x in cursor:
+        for y in x:
+            if str(ctx.author.id) in y:
+                print("I did not find you in the DB, skipping")
+                break
+
+    sql = "DELETE FROM accounts WHERE account_id = {} limit 1".format(ctx.author.id)
+    cursor.execute(sql)
     mydb.commit()
-    print(cursor.rowcount, "was inserted")
+    print(cursor.rowcount, "records deleted")
+    await ctx.send(f'I deleted {cursor.rowcount} records with the id {ctx.author.id}')
 
-# print all servers bot is connected to on startup, just for reference
-#@client.event
-#async def on_ready():
-#    print(f'{client.user} has connected to Discord!')
-#    for guild in client.guilds:
-#        print(f'{guild}')
-
-
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user: return
-#    if message.author.bot: return
-#    channel = client.get_channel(message.channel.id) # get channel id from message and use it to create channel object
-#    print (f'{channel}, {message.channel}')  # print some variables for debug
-#    textSearch = "mysql"
-#    if message.content.find(textSearch) != -1: # serach message for anywhere
-#        print( f'{message.content}\n') # print the mesage that triggered our
-        #data = cursor.execute("show databases")
-#        await channel.send('I am attached to the database: ' + db + "I can see tables: " + tables)
 
 
 bot.run(TOKEN)
-#client.run(TOKEN)
